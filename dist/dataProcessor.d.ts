5e8df744067b8e82441e7a464fd5b4e85f765dae
AngelicaGFlores <flores.guada.angelica@gmail.com>
2025-09-30 18:18:28 -0700

    Lesson 1 mod 6 class notes and project

---
+++ dist/dataProcessor.d.ts
@@ -0,0 +1,9 @@
+interface Data {
+    id: string;
+    name: string;
+    price: number;
+    discountedPrice: number;
+}
+export declare function processData(data: Data[]): Data[];
+export {};
+//# sourceMappingURL=dataProcessor.d.ts.map
---
+++ dist/dataProcessor.js
@@ -0,0 +1,23 @@
+import _ from "lodash";
+export function processData(data) {
+    let result = [];
+    for (let i = 0; i < data.length; i++) {
+        let item = data[i];
+        if (!item) {
+            throw new Error('Item is undefined');
+        }
+        if (!item.id) {
+            throw new Error("Data item is missing an id");
+        }
+        let processedItem = {
+            id: item.id,
+            name: item.name || "Unknown",
+            price: item.price || 0,
+            discountedPrice: item.discountedPrice || item.price || 0,
+        };
+        result.push(processedItem);
+    }
+    return _.orderBy(result, ["discountedPrice"], ["asc"]);
+}
+//module.exports = { processData };
+//# sourceMappingURL=dataProcessor.js.map
